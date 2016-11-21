dc94da0f7672739b6799295eeb6c4dd8cbff3c0d
b1ackit <blackit14@gmail.com>
2016-11-21 22:00:31 +0900

    Test commit & git by Hyun Ji

--- app/src/main/java/com/example/lp/lastpictures/MainActivity.java
+++ app/src/main/java/com/example/lp/lastpictures/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Test git
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
